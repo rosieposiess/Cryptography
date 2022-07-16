f5b5ed37ac808788299fab03e9b55aaf4da90be6
Haeun KIM <89526187+rosieposiess@users.noreply.github.com>
2022-07-16 15:14:48 +0900

    Create gcd.c

---
+++ RSA/gcd.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int r_gcd(int a, int b) { //recursive euclid algorithm
+
+	int max = (a > b) ? a : b;
+	int min = (a < b) ? a : b;
+	
+	if (min == 0) {
+		return max;
+	}
+	
+	else if (min == 1) {
+		return 1;
+	}
+
+	else {
+		return r_gcd(min, max%min);
+	}
+}
+
+int i_gcd(int a, int b) {
+
+	int max = (a > b) ? a : b;
+	int min = (a < b) ? a : b;
+	
+	while (min != 0) {
+		int r = max % min;
+		max = min;
+		min = r;
+	}
+	return max;
+}
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", i_gcd(a, b));
+	return 0;
+}
