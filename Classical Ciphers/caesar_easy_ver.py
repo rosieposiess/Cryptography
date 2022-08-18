c5069e5718a4246f6ac6daba57cbf06c3d129cc5
Haeun KIM <89526187+rosieposiess@users.noreply.github.com>
2022-08-18 12:06:03 +0900

    Update caesar_easy_ver.py
    upper(),lower() 문제 해결

--- Classical Ciphers/caesar_easy_ver.py
+++ Classical Ciphers/caesar_easy_ver.py
@@ -1,21 +1,21 @@
-#아직 미완
 from curses.ascii import islower, isupper
 
 def caesar_encrypt(pt,key=0):
     ct="" #이런식으로 문자열 초기화
     for c in pt:
-       if isupper(c):
-           c=islower(c)
+        if c.isupper():
+           c=c.lower() #lower() c 값 자체를 변경하지는 않음
            c=chr((ord(c)-97+key)%26+97)
-           c=isupper(c)
-       else:    
+           c=c.upper()
+           ct=ct+c
+        else:    
             c=chr((ord(c)-97+key)%26+97)
-    ct+=c
+            ct=ct+c
     return ct
+    
 
 pt=input("Enter a string you want to encrypt:")
 key=int(input("Enter your key(1~26):"))
-caesar_encrypt(pt,key)
+ct=caesar_encrypt(pt,key)
 print(ct)
-
 
